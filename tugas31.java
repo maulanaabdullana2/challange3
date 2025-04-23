163b13d369c8e4767dbfa498d2e1e321419328c5
maulana <maulanaabdullana123@gmail.com>
2025-04-23 10:52:33 +0700

    first commit

--- tugas31.java
+++ tugas31.java
@@ -248,11 +248,11 @@ public class tugas31 {
         for (int i = 0; i < s.length(); i++) {
             for (int j = i+1; j < s.length() ; j++) {
                 if (s.charAt(i) == s.charAt(j)){
-                    return true;
+                    return false;
                 }
             }
         }
-        return false;
+        return true;
     }
 
 
